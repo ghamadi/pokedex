3b539349ea40cc76df27bb655f84150dc7737e4b
Ghaleb Hamadi <ghaleb.hamadi@outlook.com>
2022-11-26 15:03:06 +0200

    wrap fetching methods with cache

--- src/api/pokemon.ts
+++ src/api/pokemon.ts
@@ -1,11 +1,10 @@
 import axios from 'axios';
+import { cache } from 'react';
 import { ListApiResponse, PokemonApiResponse } from '../../poke-api-types';
 
-export async function getPokemonList(offset: number, limit?: number): Promise<ListApiResponse> {
+export const getPokemonList = cache(async (offset: number, limit?: number): Promise<ListApiResponse> => {
   let limitParam = limit ? `limit=${limit}` : '';
   return (await axios.get(`${process.env.NEXT_PUBLIC_POKE_API_POKEMON}/?${limitParam}&offset=${offset}`)).data;
-}
+});
 
-export async function getPokemon(url: string): Promise<PokemonApiResponse> {
-  return (await axios.get(url)).data;
-}
+export const getPokemon = cache(async (url: string): Promise<PokemonApiResponse> => (await axios.get(url)).data);
